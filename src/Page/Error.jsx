7eaf114551c3ef5d56b1e0e679b0099571e8d8cc
fojletuhin <fojletuhin@gmail.com>
2024-05-11 21:11:49 +0600

    add lottie in error page

--- src/Page/Error.jsx
+++ src/Page/Error.jsx
@@ -1,8 +1,16 @@
+import Lottie from "lottie-react";
+import ErrorImg from "../assets/ErrorImg.json";
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
         <div>
-            <p>I am from error page</p>
+            <div className="flex justify-center items-center">
+                <Lottie animationData={ErrorImg} className="h-[500px]"/>
+            </div>
+            <div className="w-1/2 m-auto  mt-10">
+                <Link to='/'><button className="btn bg-[#87909A] flex mt-[-60px] m-auto font-bold text-white text-2xl">Back to home</button></Link>
+            </div>
         </div>
     );
 };
