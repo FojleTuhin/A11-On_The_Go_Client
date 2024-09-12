8a060ea95327fef853b2a8b097460f9efa76e507
fojletuhin <fojletuhin@gmail.com>
2024-09-12 23:57:48 +0600

    add loading skeleton

--- src/Component/RecentBlog.jsx
+++ src/Component/RecentBlog.jsx
@@ -5,30 +5,54 @@ import axios from "axios";
 
 const RecentBlog = () => {
 
-    
 
 
-    const { data = [] } = useQuery({
+
+    const { data = [], isLoading } = useQuery({
         queryFn: () => getData(),
         queryKey: ['feature']
 
     })
 
-    
+
     const getData = async () => {
         const { data } = await axios(`https://on-the-go-server.vercel.app/feature`)
         return data
     }
 
 
-    
+
+    if (isLoading) {
+        return (
+            <div className="min-h-screen flex justify-center items-center gap-7">
+                <div className="flex w-52 flex-col gap-4">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+                <div className="flex w-52 flex-col gap-4">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+                <div className="flex w-52 flex-col gap-4">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+            </div>
+        )
+    }
 
 
     return (
         <div className='bg-[#F8F6F1] px-4 md:px-8 lg:px-[70px] pb-10  pt-48 flex justify-center flex-wrap gap-6'>
-            
+
             {
-                data.slice(0,6).map(item => <CardUi key={item._id} item={item}></CardUi>)
+                data.slice(0, 6).map(item => <CardUi key={item._id} item={item}></CardUi>)
             }
         </div>
     );
