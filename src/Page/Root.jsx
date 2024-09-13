c2f9ded2eb2eca4c1cfd1a74084d7d2f4a460bce
fojletuhin <fojletuhin@gmail.com>
2024-09-13 23:46:13 +0600

    remove navbar and footer from login register page

--- src/Page/Root.jsx
+++ src/Page/Root.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Component/Footer";
 import Navbar from "../Component/Navbar";
 import ScrollToTop from "../Component/ScrollToTop";
 
 const Root = () => {
+
+    const location = useLocation()
+    const isLogin = location.pathname.includes('login') || location.pathname.includes('register') 
     return (
         <div>
             <ScrollToTop></ScrollToTop>
-            <Navbar></Navbar>
+            {isLogin || <Navbar></Navbar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            {isLogin || <Footer></Footer>}
 
         </div>
     );
