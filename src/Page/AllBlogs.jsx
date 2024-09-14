1ca5190554aea08e73a5cd8ea4124477d281b116
fojletuhin <fojletuhin@gmail.com>
2024-09-14 12:09:32 +0600

    update all blog page

--- src/Page/AllBlogs.jsx
+++ src/Page/AllBlogs.jsx
@@ -12,8 +12,8 @@ const AllBlogs = () => {
 
     const { data } = useQuery({
         queryFn: () => getData(),
-        queryKey: ["blogs",filter, search],
-        initialData:[]
+        queryKey: ["blogs", filter, search],
+        initialData: []
 
     })
     const getData = async () => {
@@ -22,7 +22,7 @@ const AllBlogs = () => {
     }
 
 
-    const handleSearch=(e)=>{
+    const handleSearch = (e) => {
         e.preventDefault()
         const searchText = e.target.title.value;
         setSearch(searchText)
@@ -35,47 +35,49 @@ const AllBlogs = () => {
 
         <div className=''>
             <img src={img1} className='h-[300px] w-full' alt="" />
-            <div className="bg-[#EBFBE5]  text-[#3EA570] py-4">
-                <h1 className="font-bold text-xl text-center">All Blog</h1>
-            </div>
-
-            <div className='px-4 md:px-8 lg:px-[100px] mb-6 mt-5 flex justify-between'>
-                <div className="form-control md:w-1/4">
-                    <label className="label">
-                        <span className="label-text text-black font-bold">Search By Category</span>
-                    </label>
-
-                    <select
-                    onChange={e=>setFilter(e.target.value)}
-                    
-                    className="select w-full bg-none bg-[#f4f3f0] text-black border-black" name="category" id='category' value={filter}>
-                        <option disabled selected> Category</option>
-                        <option>City Travel</option>
-                        <option>Adventure Travel</option>
-                        <option>Wildlife Travel</option>
-                        <option>Beach Travel</option>
-                        <option>Cultural Travel</option>
-                    </select>
+            <div className='max-w-6xl mx-auto mb-10  -mt-32 relative '>
+                <div className="bg-[#EBFBE5]  text-[#3EA570] py-4 rounded-t-lg">
+                    <h1 className="font-bold text-xl text-center">All Blog</h1>
                 </div>
-                <form className='flex' onSubmit={handleSearch}>
-                    <div className="form-control ">
+
+                <div className='px-4 md:px-8 lg:px-[100px] pb-6 pt-5 flex justify-between bg-[#F8F6F1]'>
+                    <div className="form-control md:w-1/4 ">
                         <label className="label">
-                            <span className="label-text text-black font-bold">Search By Title</span>
+                            <span className="label-text text-black font-bold">Search By Category</span>
                         </label>
-                        <label className="input-group">
-                            <input type="text" name="title" required placeholder="Title" className="input input-bordered w-full bg-none bg-[#f4f3f0] text-black border-black" />
-                        </label>
+
+                        <select
+                            onChange={e => setFilter(e.target.value)}
+
+                            className="select w-full bg-none  text-black border-black" name="category" id='category' value={filter}>
+                            <option disabled selected> Category</option>
+                            <option>City Travel</option>
+                            <option>Adventure Travel</option>
+                            <option>Wildlife Travel</option>
+                            <option>Beach Travel</option>
+                            <option>Cultural Travel</option>
+                        </select>
                     </div>
-                    <button className='btn mt-9'>Search</button>
-                </form>
+                    <form className='flex' onSubmit={handleSearch}>
+                        <div className="form-control ">
+                            <label className="label">
+                                <span className="label-text text-black font-bold">Search By Title</span>
+                            </label>
+                            <label className="input-group">
+                                <input type="text" name="title" required placeholder="Title" className="input input-bordered w-full bg-none  text-black border-black border-r-0 " />
+                            </label>
+                        </div>
+                        <button className='btn mt-9 -ml-5 text-white border-none bg-pink-700 hover:text-black'>Search</button>
+                    </form>
 
 
 
-            </div>
-            <div className='bg-[#F8F6F1] px-4 md:px-8 lg:px-[70px] pb-10  pt-10 flex justify-center flex-wrap gap-6'>
-                {
-                    data.map(item => <CardUi key={item._id} item={item}></CardUi>)
-                }
+                </div>
+                <div className='bg-[#F8F6F1] px-4 md:px-8 lg:px-[70px] pb-10  pt-10 flex justify-center flex-wrap gap-6 rounded-b-lg'>
+                    {
+                        data.map(item => <CardUi key={item._id} item={item}></CardUi>)
+                    }
+                </div>
             </div>
         </div>
     );
